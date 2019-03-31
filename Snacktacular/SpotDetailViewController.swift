b200d414ceb997d532b30b57de5e7cf9a1c6264a
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-03-31 19:46:11 -0400

    9.8 Complete

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import GooglePlaces
 import MapKit
+import Contacts
 
 class SpotDetailViewController: UIViewController {
     
@@ -22,6 +23,9 @@ class SpotDetailViewController: UIViewController {
     
     var spot: Spot!
     let regionDistance: CLLocationDistance = 750
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +34,9 @@ class SpotDetailViewController: UIViewController {
         
         if spot == nil {
             spot = Spot()
+            getLocation()
         }
         updateUserInterface()
-        
         let region = MKCoordinateRegion(center: spot.coordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
         mapView.setRegion(region, animated: true)
     }
@@ -66,6 +70,8 @@ class SpotDetailViewController: UIViewController {
     }
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        spot.name = nameField.text!
+        spot.address = addressField.text!
         spot.saveData { success in
             if success {
                 self.leaveViewController()
@@ -119,4 +125,58 @@ extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
+}
+
+extension SpotDetailViewController: CLLocationManagerDelegate {
+    func getLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func handleLocationAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied:
+            print("I'm sorry - can't show location. User has not authorized it.")
+        case .restricted:
+            print("Access denied - Likely parental controls are restricting location services in this app")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard spot.name == "" else {
+            return
+        }
+        currentLocation = locations.last
+        let geoCoder = CLGeocoder()
+        var name = ""
+        var address = ""
+        spot.coordinate = currentLocation.coordinate
+        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
+            {placemarks, error in
+                if placemarks != nil{
+                    let placemark = placemarks?.last
+                    name = placemark?.name ?? "Name Unknown"
+                    if let postalAddress = placemark?.postalAddress {
+                        address = CNPostalAddressFormatter.string(from: postalAddress, style: .mailingAddress)
+                    }
+                } else {
+                    print("Error retrieving place. Error code \(error!)")
+                }
+                self.spot.name = name
+                self.spot.address = address
+                self.updateUserInterface()
+        })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location")
+    }
 }
